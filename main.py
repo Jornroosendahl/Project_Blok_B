6a5862afbbf49a7c18a23558cb025bf079404829
Jornroosendahl <33836001+Jornroosendahl@users.noreply.github.com>
2021-12-09 14:09:43 +0100

    Changed + to -

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def myfunction(a,b):
-    return a+b
+    return a-b
 
 if __name__ == '__main__':
     print_hi('Jorn')
