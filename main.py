77c2720be6ab2862f3319b6fd7884c795e362dee
Jorn <jornroosendahl@hotmail.com>
2021-12-09 14:01:45 +0100

    Created new function

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def myfunction(a,b):
+    return a+b
 
 if __name__ == '__main__':
     print_hi('Jorn')
 
 print("How are you?")
+
+print(myfunction(2,4))
