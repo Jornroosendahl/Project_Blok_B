e67faeaaecd7c21adaa1ed904e10ff242a856c94
Jorn <jornroosendahl@hotmail.com>
2021-12-20 14:18:51 +0100

    Eerste begin: .json wordt geladen

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ import json
 with open('steam.json') as json_file:
     data = json.load(json_file)
 
-print(data[0])
-print('hoi')
-print('Test')
-print('waaa')
-print('Hoi')
-print('test')
+print(data[1])
