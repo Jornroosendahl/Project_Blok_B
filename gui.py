fae48c13a338c39323b313078cf8e3385621bf15
Jorn <jornroosendahl@hotmail.com>
2022-01-03 16:20:46 +0100

    update GUI

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from main import *
 
@@ -5,6 +6,8 @@ from main import *
 root = Tk()
 #window title
 root.title('Steam')
+#window icon
+root.iconphoto(False,tkinter.PhotoImage(file='Steam.png'))
 #resizing is false
 root.resizable(width=False,height=False)
 #set window size
@@ -16,27 +19,54 @@ var_naam.set(eerste_spel_naam())
 var_info = StringVar()
 var_info.set(eerste_spel_info())
 
-label_naam = Label(master=root,
-                   textvariable=var_naam,
-                   background='white')
-label_naam.pack()
+def info():
+    label_naam = Label  (
+                    master=root,
+                    textvariable=var_naam,
+                    background='white',
+                    font='Helvetica 25 bold'
+                    )
+    label_naam.grid(
+                    row=0,
+                    column=0,
+                    sticky=''
+                    )
+    label_info = Message(
+                    master=root,
+                    background='white',
+                    width=180,
+                    textvariable=var_info
+                    )
+    label_info.grid     (
+                    row=0,
+                    column=1,
+                    sticky=''
+                    )
 
-label_info = Message(master=root,
-                   textvariable=var_info,
-                   background='white')
-label_info.pack()
-
-
-left_button = Button(master=root,
-                     text='Left',
-                     command = '',
-                     )
-left_button.pack(side=LEFT)
-
-right_button = Button(master=root,
-                     text='Right',
-                     command = '')
-right_button.pack(side=RIGHT)
+def volgende():
+    info()
+    global teller
+    teller +=1
+left_button = Button(
+                    master=root,
+                    text='Back',
+                    command = '',
+                    )
+left_button.grid    (
+                    row=1,
+                    column=0,
+                    sticky='nes'
+                    )
+right_button = Button(
+                    master=root,
+                    text='Next',
+                    command = info
+                    )
+right_button.grid   (
+                    row=1,
+                    column=1,
+                    sticky='w'
+                    )
 
 #show window
 mainloop()
--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import json
 with open('steam.json') as json_file:
     data = json.load(json_file)
 
-
+teller = 0
 def eerste_spel_naam():
-    spel = data[0]
+    spel = data[teller]
     for i in spel:
         if 'name' in i:
             naam = spel[i]
     return naam
 
 def eerste_spel_info():
-    spel = data[0]
+    spel = data[teller]
     for i in spel:
         if 'appid' in i:
             app_id = 'AppID:' + str(spel[i])
@@ -20,12 +20,12 @@ def eerste_spel_info():
             release_date = 'Release_date:' + str(spel[i])
         if 'genres' in i:
             genre = 'Genre:' + str(spel[i])
-    return app_id,release_date,genre
+        if 'price' in i:
+            price = 'Price:' + str(spel[i])
+    info = app_id + '\n' + release_date + '\n' + genre + '\n' + price
+    return info
 
 def steam_sort():
     lijst = sorted(data,key=lambda i: i['price'])
     #,reverse=True om om te keren --- price veranderen in iets anders om daarop te sorteren
     return lijst
-
-
-
