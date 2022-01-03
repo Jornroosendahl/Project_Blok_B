14bc2ede6d337e39f34fc8e83160696085e21e1f
Jorn <jornroosendahl@hotmail.com>
2022-01-03 13:38:40 +0100

    Extra functie gemaakt die meer info toont in de gui

--- gui.py
+++ gui.py
@@ -10,24 +10,33 @@ root.resizable(width=False,height=False)
 #set window size
 root.geometry('500x400')
 
-var = StringVar()
-var.set(eerste_spel_naam())
+var_naam = StringVar()
+var_naam.set(eerste_spel_naam())
 
+var_info = StringVar()
+var_info.set(eerste_spel_info())
 
 label_naam = Label(master=root,
-                   textvariable=var,
+                   textvariable=var_naam,
                    background='white')
 label_naam.pack()
 
+label_info = Message(master=root,
+                   textvariable=var_info,
+                   background='white')
+label_info.pack()
+
+
 left_button = Button(master=root,
                      text='Left',
-                     command = '')
-left_button.pack()
+                     command = '',
+                     )
+left_button.pack(side=LEFT)
 
 right_button = Button(master=root,
                      text='Right',
                      command = '')
-right_button.pack()
+right_button.pack(side=RIGHT)
 
 #show window
 mainloop()
--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import json
 with open('steam.json') as json_file:
     data = json.load(json_file)
 
+
 def eerste_spel_naam():
     spel = data[0]
     for i in spel:
         if 'name' in i:
             naam = spel[i]
     return naam
+
+def eerste_spel_info():
+    spel = data[0]
+    for i in spel:
+        if 'appid' in i:
+            app_id = 'AppID:' + str(spel[i])
+        if 'release_date' in i:
+            release_date = 'Release_date:' + str(spel[i])
+        if 'genres' in i:
+            genre = 'Genre:' + str(spel[i])
+    return app_id,release_date,genre
 
 def steam_sort():
     lijst = sorted(data,key=lambda i: i['price'])
