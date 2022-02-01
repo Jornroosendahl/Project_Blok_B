bfaded84272fbe4abdc37d58aebda3a19aacd3de
Jorn <jornroosendahl@hotmail.com>
2022-02-01 12:04:33 +0100

    Meerdere functies en knoppen toegevoegd: -Frequentieveld, hier geef je een opdracht om te kijken hoe vaak een waarde voorkomt bij een bepaalde key. Bijvoorbeeld: developer, price etc
    -Range, deze knop laat het verschil in prijs zien tussen het goedkoopste spel en het duurste spel
    
    - Sorteerveld, deze knop geeft een lijst terug met daarin de games gesorteerd op een bepaalde waarde die in het entryveld wordt ingevuld. Bijvoorbeeld appid, price, name.
    
    Gemaakt door Enock en Bekir

--- gui.py
+++ gui.py
@@ -3,20 +3,22 @@ from main import *
 from RPI import *
 import sys
 import tkinter.messagebox as messagebox
+
 with open('steam.json') as json_file:
     data = json.load(json_file)
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-#base window
+# base window
 root = Tk()
-#window title
+# window title
 root.title('Steam')
-#window icon
-#root.iconphoto(False,tkinter.PhotoImage(file='Steam.png'))
-#resizing is false
-root.resizable(width=True,height=False)
-#set window size
-root.geometry('800x400')
+# window icon
+# root.iconphoto(False,tkinter.PhotoImage(file='Steam.png'))
+# resizing is false
+root.resizable(width=True, height=False)
+# set window size
+root.geometry('800x800')
+
 
 class check_button(Thread):
 
@@ -46,49 +48,56 @@ def hide_show():
     show_info()
 
 
-
 var_naam = StringVar()
 var_naam.set(eerste_spel_naam())
 
 var_info = StringVar()
 var_info.set(eerste_spel_info())
 
-label_naam = Label  (
-                    master=root,
-                    textvariable=var_naam,
-                    background='white',
-                    font='Helvetica 25 bold'
-                    )
+label_naam = Label(
+    master=root,
+    textvariable=var_naam,
+    background='white',
+    font='Helvetica 25 bold'
+)
 label_info = Message(
-                    master=root,
-                    background='white',
-                    width=180,
-                    textvariable=var_info
-                    )
+    master=root,
+    background='white',
+    width=180,
+    textvariable=var_info
+)
+
+
 def show_info():
     var_naam.set(eerste_spel_naam())
     label_naam.config(textvariable=var_naam)
     var_info.set(eerste_spel_info())
     label_info.config(textvariable=var_info)
     label_naam.grid(
-                    row=0,
-                    column=0,
-                    sticky=''
-                    )
-    label_info.grid (
-                    row=0,
-                    column=1,
-                    sticky=''
-                    )
+        row=0,
+        column=0,
+        sticky=''
+    )
+    label_info.grid(
+        row=0,
+        column=1,
+        sticky=''
+    )
     global teller
-    teller +=1
+    teller += 1
+
+
 def hide_info():
     label_naam.grid_forget()
     label_info.grid_forget()
+
+
 def hide_show():
     hide_info()
     show_info()
     hc595(rating())
+
+
 def entry_waarde():
     waarde = zoekveld.get()
     return waarde
@@ -102,16 +111,14 @@ def search():
                 if entry_waarde() in i[x] or entry_waarde() in str(i[x]).lower():
                     try:
                         label_zoeknaam.destroy()
-                        #label_zoekinfo.destroy()
+                        # label_zoekinfo.destroy()
                     except:
                         print('Nothing to destroy')
                     label_zoeknaam = Label(master=root,
                                            text=i[x],
-                                           background = 'white',
+                                           background='white',
                                            font='Helvetica 25 bold')
                     zoek_naam = i[x]
-
-
 
                     label_zoeknaam.grid(row=7,
                                         column=0,
@@ -141,9 +148,70 @@ def search():
                     #                     sticky='w')
                     messagebox.showinfo(zoek_naam, zoek_info)
 
+
 def forget():
     global label_zoeknaam
     label_zoeknaam.destroy()
+
+
+def Entry_waarde():
+    waarde = sorteerveld.get()
+    return waarde
+
+def my_sort():
+    global label_zoeknaam
+    newlst = data[0:50]
+    Jewan = Entry_waarde()
+    text = ('')
+    while True:
+        #Controle variabele aanmaken.
+        check = 0
+        #Voor elke getal van 0 tot getal voor input, dan gaat het door de conditie.
+        for i in range(0, len(newlst) - 1):
+            #Als de index van newlst[i] groter is dan newlst[i+1]
+            if newlst[i][Jewan] > newlst[i + 1][Jewan]:
+                #Verander ze dan van plek/positie
+                newlst[i], newlst[i + 1] = newlst[i + 1], newlst[i]
+                check += 1
+        #Als de check gelijk is aan 0, return newlst
+        if check == 0:
+            for b in newlst:
+                text = text + str(b['name']) + '\n'
+                if Jewan != 'name':
+                    text = text + str(b[Jewan]) + '\n'
+            break
+    messagebox.showinfo('gesorteerd', text)
+def Entrywaarde():
+    waarde = frequentieveld.get()
+    return waarde
+
+def freq():
+    global label_zoeknaam
+    newlst = data[0:50]
+    Jewan = Entrywaarde()
+    freq = {}
+    for i in newlst:
+        if (i[Jewan] in freq):  # hierin wordt er gekeken of er een i in freq voorkomt.
+            freq[i[Jewan]] += 1
+        if i[Jewan] not in freq:  # Als de i niet in de freq voorkomt.
+            freq[i[Jewan]] = 1
+    messagebox.showinfo('frequentie', freq)
+     # Hiermee wordt de waarde frequenties gereturnd.
+
+def rnge():
+    global label_zoeknaam
+    newlst = data
+    temp = 0
+    for i in newlst:
+        if i['price'] > temp:
+            temp = i['price']
+    temp2 = sys.maxsize
+    for i in newlst:
+        if i['price'] < temp2:
+            temp2 = i['price']
+    messagebox.showinfo('range:',temp - temp2)
+
+
 
 
 next_button = Button(
@@ -173,6 +241,63 @@ zoekknop.grid       (row=5,
                      column=0,
                      sticky='w')
 
+sorteerveld = Entry(
+                    master=root,
+
+                   )
+sorteerveld.grid   (
+                    row=35,
+                    column=0,
+                    sticky='w'
+                    )
+sorteerknop = Button(master=root,
+                     text='sort',
+                     command=my_sort)
+sorteerknop.grid    (row=40,
+                     column=0,
+                     sticky='w'
+                    )
+frequentieveld = Entry(
+                      master=root,
+                      )
+frequentieveld.grid (
+                     row=10,
+                     column=0,
+                     sticky='w'
+)
+# rngeveld = Entry(
+#                  master=root
+#                  )
+# rngeveld.grid (
+#                row=14,
+#                column=0,
+#                sticky='w'
+#               )
+rngeknop = Button(master=root,
+                  text='range',
+                  command=rnge)
+rngeknop.grid   (
+                row=15,
+                column=0,
+                sticky='w'
+                )
+frequentieknop = Button(master=root,
+                        text='frequentie',
+                        command=freq
+                        )
+frequentieknop.grid     (
+                        row=12,
+                        column=0,
+                        sticky='w'
+                        )
+label_rngeveld = Label(
+                   master=root,
+                   text='range:')
+label_rngeveld.grid(
+                    row=13,
+                    column=0,
+                    sticky='w')
+
 label_zoekveld = Label(
                     master=root,
                     text='Zoekveld:')
@@ -180,42 +305,52 @@ label_zoekveld.grid (row=3,
                      column=0,
                      sticky='w')
 
-#show window
-#mainloop()
+label_sorteerveld = Label(
+                    master=root,
+                    text='Sorteerveld:')
+label_sorteerveld.grid (row=30,
+                        column=0,
+                        sticky='w')
+label_frequentieveld = Label(
+                            master=root,
+                            text='Frequentieveld:')
+label_frequentieveld.grid (row=9,
+                           column=0,
+                           sticky='w')
 
 # het importeren van de gespecificeerde module
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # x aswaarden
 
 
-#x = [10,20,30,40,50,60,70,80,130,220,240,280,300,320,340,360,380,400,420,440,500,550,570,620,630,730,1002,1200]
+# x = [10,20,30,40,50,60,70,80,130,220,240,280,300,320,340,360,380,400,420,440,500,550,570,620,630,730,1002,1200]
 
 # corresponderende y-aswaarden
 
-#y = [7.19,3.99,3.99,3.99,3.99,3.99,7.19,7.19,3.99,7.19,7.19,0.0,7.19,3.99,0.0,0.0,5.79,7.19,5.79,0.0,7.19,7.19,0.0,7.19,0.0,0.0,5.99,3.99,]
+# y = [7.19,3.99,3.99,3.99,3.99,3.99,7.19,7.19,3.99,7.19,7.19,0.0,7.19,3.99,0.0,0.0,5.79,7.19,5.79,0.0,7.19,7.19,0.0,7.19,0.0,0.0,5.99,3.99,]
 
 # het plotten van de punten
 
-#plt.plot(x, y)
+# plt.plot(x, y)
 
 # het benoemen van de x-as
 
-#plt.xlabel('x – as')
+# plt.xlabel('x – as')
 
 # het benoemen van de y-as
 
-#plt.ylabel('y – as')
+# plt.ylabel('y – as')
 
 # het geven van een titel aan mijn grafiek
 
-#plt.title('Verhouding prijs en appid')
+# plt.title('Verhouding prijs en appid')
 
 # functie om de plot aan te duiden
 
-#plt.show()
+# plt.show()
 
-#show window
+# show window
 chk1 = check_button()
 c1 = Thread(target=chk1.checkloop)
 c1.start()
@@ -227,11 +362,13 @@ mainloop()
 # x aswaarden
 
 
-x = [10,20,30,40,50,60,70,80,130,220,240,280,300,320,340,360,380,400,420,440,500,550,570,620,630,730,1002,1200]
+x = [10, 20, 30, 40, 50, 60, 70, 80, 130, 220, 240, 280, 300, 320, 340, 360, 380, 400, 420, 440, 500, 550, 570, 620,
+     630, 730, 1002, 1200]
 
 # corresponderende y-aswaarden
 
-y = [317,62,34,184,415,10,83,43,205,402,400,214,134,32,81,184,137,301,623,278,566,801,520,83,6502,0,258,0]
+y = [317, 62, 34, 184, 415, 10, 83, 43, 205, 402, 400, 214, 134, 32, 81, 184, 137, 301, 623, 278, 566, 801, 520, 83,
+     6502, 0, 258, 0]
 
 # het plotten van de punten
 
